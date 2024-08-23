9f9df42338af9b0d3e01c32126510ad04a22718c
Elle-Lo <tzuningluo@gmail.com>
2024-08-23 15:30:08 +0800

    Create tab bar

--- Reflix/ContentView.swift
+++ Reflix/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectedTab = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        TabView(selection: $selectedTab) {
+            
+            MovieTabView()
+                .tabItem {
+                    Label("movies", image: "movie")
+                }.tag(1)
+            
+            SearchTabView()
+                .tabItem {
+                    Label("search", image: "magnifier")
+                }.tag(2)
         }
-        .padding()
+        .accentColor(.black)
+    }
+    
+}
+
+struct MovieTabView: View {
+    var body: some View {
+        Text("Movie")
+    }
+}
+
+struct SearchTabView: View {
+    var body: some View {
+        Text("Search")
     }
 }
 
 #Preview {
     ContentView()
+}
+
+#Preview("ContentView (Landscape)", traits: .landscapeLeft) {
+ContentView()
 }
---
+++ Reflix/TabView.swift
@@ -0,0 +1,8 @@
+//
+//  TabView.swift
+//  Reflix
+//
+//  Created by Tzu ning Lo on 2024/8/23.
+//
+
+import Foundation
