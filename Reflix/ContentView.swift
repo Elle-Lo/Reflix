e2c12a26404a1466577fa38068d96029125dd1f5
Elle-Lo <tzuningluo@gmail.com>
2024-08-23 15:37:17 +0800

    Put TabView in TabView File

--- Reflix/ContentView.swift
+++ Reflix/ContentView.swift
@@ -1,46 +1,21 @@
-//
-//  ContentView.swift
-//  Reflix
-//
-//  Created by Tzu ning Lo on 2024/8/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     
-    @State var selectedTab = 0
-    
+
     var body: some View {
-        
-        TabView(selection: $selectedTab) {
-            
-            MovieTabView()
-                .tabItem {
-                    Label("movies", image: "movie")
-                }.tag(1)
-            
-            SearchTabView()
-                .tabItem {
-                    Label("search", image: "magnifier")
-                }.tag(2)
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
-        .accentColor(.black)
+        .padding()
+
     }
     
 }
 
-struct MovieTabView: View {
-    var body: some View {
-        Text("Movie")
-    }
-}
-
-struct SearchTabView: View {
-    var body: some View {
-        Text("Search")
-    }
-}
 
 #Preview {
     ContentView()
--- Reflix/TabView.swift
+++ Reflix/TabView.swift
@@ -1,8 +1,46 @@
-//
-//  TabView.swift
-//  Reflix
-//
-//  Created by Tzu ning Lo on 2024/8/23.
-//
+import SwiftUI
 
-import Foundation
+struct TabBarView: View {
+    
+    @State var selectedTab = 0
+    
+    var body: some View {
+        
+        TabView(selection: $selectedTab) {
+            
+            MovieTabView()
+                .tabItem {
+                    Label("movies", image: "movie")
+                }.tag(1)
+            
+            SearchTabView()
+                .tabItem {
+                    Label("search", image: "magnifier")
+                }.tag(2)
+        }
+        .accentColor(.black)
+    }
+    
+}
+
+//Movie頁的畫面
+struct MovieTabView: View {
+    var body: some View {
+        Text("Movie")
+    }
+}
+
+//Search頁的畫面
+struct SearchTabView: View {
+    var body: some View {
+        Text("Search")
+    }
+}
+
+#Preview {
+    TabBarView()
+}
+
+#Preview("ContentView (Landscape)", traits: .landscapeLeft) {
+    TabBarView()
+}
