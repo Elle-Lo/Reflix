362554d9bcb218a525505a38926ba9ecd2571ff5
Elle-Lo <tzuningluo@gmail.com>
2024-08-23 09:55:57 +0800

    Establish .gitignore

---
+++ Reflix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reflix
+//
+//  Created by Tzu ning Lo on 2024/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reflix/ReflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReflixApp.swift
+//  Reflix
+//
+//  Created by Tzu ning Lo on 2024/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ReflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
