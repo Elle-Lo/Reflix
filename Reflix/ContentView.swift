23f8664ba367012a310689a044b4475432b5c514
joey <unjayintw@gmail.com>
2024-08-24 22:01:28 +0800

    add test to merge file

---
+++ Reflix/ContentView.swift
@@ -0,0 +1,21 @@
+
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        TabBarView()
+        
+    }
+       
+}
+
+
+#Preview {
+    ContentView()
+}
+
+//#Preview("ContentView (Landscape)", traits: .landscapeLeft) {
+//ContentView()
+//}
