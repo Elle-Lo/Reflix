9efd7a4999c53b2dd070583bc4d1b85255fa09bc
Elle-Lo <tzuningluo@gmail.com>
2024-08-27 15:55:38 +0800

    File push

--- Reflix/MovieDetailView2.swift
+++ Reflix/MovieDetailView2.swift
@@ -1,21 +1,28 @@
+//
+//  MovieDetailView2.swift
+//  Reflix
+//
+//  Created by 池昀哲 on 2024/8/27.
+//
+
 import SwiftUI
 
 struct MovieDetailView2: View {
     let movie: MovieSearchResult
-    
+
     var body: some View {
         VStack {
             Text(movie.title)
                 .font(.largeTitle)
                 .padding()
-            
+
             if let releaseDate = movie.release_date {
                 Text("Release Date: \(releaseDate)")
                     .font(.title2)
                     .foregroundColor(.gray)
                     .padding()
             }
-            
+
             Spacer()
         }
         .background(Color.black.ignoresSafeArea())
@@ -24,5 +31,5 @@ struct MovieDetailView2: View {
 }
 
 //#Preview {
-//    MovieDetailView(movie: MovieSearchResult)
+//    MovieDetailView2()
 //}
